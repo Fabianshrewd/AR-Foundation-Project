11a75ea8467e245fc4aa8cb4c15c0f5a8b4b576f
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-10-02 19:37:25 +0200

    Added the System.Text.Json library

--- Assets/Scripts/ChangeText2.cs
+++ Assets/Scripts/ChangeText2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Text.Json;
 
 public class ChangeText2 : MonoBehaviour
 {
@@ -17,14 +18,28 @@ public class ChangeText2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Get the information
+        //Get the information from the weasel webserver
         System.Net.WebClient wc = new System.Net.WebClient();
-        byte[] raw = wc.DownloadData("http://fabiangranig.at/info2.txt");
+
+        //Main address: http://10.0.9.22:4567/weasels //Backup address: https://fabiangranig.at/weasels
+        byte[] raw = wc.DownloadData("https://fabiangranig.at/weasels");
 
         //Convert in an string
         string webData = System.Text.Encoding.UTF8.GetString(raw);
 
+        //Create the JSON Document
+        using JsonDocument doc = JsonDocument.Parse(webData);
+        JsonElement root = doc.RootElement;
+
+        //Weasels aufteilen
+        var u1 = root[0];
+        var u2 = root[1];
+        var u3 = root[2];
+
+        //Create string values
+        string sol = "Name: " + u1.GetProperty("weaselId") + "\n" + "last Waypoint: " + u1.GetProperty("lastWaypoint");
+
         //Put the text on the gameobject
-        myText.text = webData;
+        myText.text = sol;
     }
 }
